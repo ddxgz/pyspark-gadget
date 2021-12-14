4a1e9757d996a831f28788424c6299d4db1dc7cb
ddxgz <ddxgz19880813@gmail.com>
2021-12-14 15:52:39 +0100

    Added a few util methods to file data source

--- gadget/datasources.py
+++ gadget/datasources.py
@@ -2,6 +2,9 @@ import os
 from abc import ABC, abstractmethod
 from typing import Optional
 
+from pyspark.sql import DataFrame
+from delta.tables import DeltaTable
+
 # TODO
 # ds.isDeltaTable
 # ds.readDelta
@@ -27,11 +30,39 @@ class FileDataSource(DataSource):
         pass
 
     def path(self, to: str) -> str:
-        return compose_path(self.root_path(), to)
+        return compose_path(self.root_path, to)
 
     def __str__(self) -> str:
         return f"[{self.source_type}] {self.root_path()}"
+    
+    def ls(self, sub_dir: str=None):
+        _dir = compose_path(self.root_path, sub_dir) if sub_dir else self.root_path
+        return dbutils.fs.ls(_dir)
 
+        
+    def read(self, format: str="delta", options: dict={}):
+        return spark.read.format(format).options(**options).load(self.root_path)
+    
+    def read_delta(self, options: dict={}):
+        if not DeltaTable.isDeltaTable(spark, self.root_path):
+            print("Not a delta table!! Will return a None")
+            return None      
+        
+        return self.read(format="delta", options=options)
+            
+    def write(self, df: DataFrame, mode:str, format: str="delta", options: dict={}):
+        return df.write.mode(mode).format(format).options(**options).save(self.root_path)
+    
+    def write_delta(self, df: DataFrame, mode:str, options: dict={}):
+        return self.write(df, format="delta", mode=mode, options=options)
+    
+    def delta_table(self):
+        if not DeltaTable.isDeltaTable(spark, self.root_path):
+            print("Not a delta table!! Will return a None")
+            return None      
+        
+        return DeltaTable.forPath(spark, self.root_path)
+    
 
 class DataSourceLocal(FileDataSource):
     def __init__(self, path_prefix: Optional[str] = None):
